a1e2fc7a4b3c4f75ed3a559b26e04d60af533e70
Matas Vosylius <37812778+matasvosylius@users.noreply.github.com>
2021-05-10 23:55:07 +0300

    Added mainnet Network config

--- src/config/NetworkConfig.ts
+++ src/config/NetworkConfig.ts
@@ -81,9 +81,41 @@ export const defaultTestnetNetworkConfig: NetworkConfig = {
     ],
 };
 
+export const defaultMainnetNetworkConfig: NetworkConfig = {
+    explorerUrl: 'http://e.twix.live',
+    faucetUrl: 'https://f.twix.live',
+    defaultNetworkType: 104,
+    networkConfigurationDefaults: {
+        maxMosaicDivisibility: 8,
+        namespaceGracePeriodDuration: 60,
+        lockedFundsPerAggregate: '10000000',
+        maxCosignatoriesPerAccount: 25,
+        blockGenerationTargetTime: 30,
+        maxNamespaceDepth: 3,
+        maxMosaicDuration: 315360000,
+        minNamespaceDuration: 60,
+        maxNamespaceDuration: 6307200,
+        maxTransactionsPerAggregate: 100,
+        maxCosignedAccountsPerAccount: 25,
+        maxMessageSize: 4096,
+        maxMosaicAtomicUnits: 100000000000000,
+        currencyMosaicId: '7831128EAA6516FC',
+        harvestingMosaicId: '7831128EAA6516FC',
+        defaultDynamicFeeMultiplier: 100,
+        epochAdjustment: 1514757600,
+        totalChainImportance: 1000000000000000,
+        generationHash: '9249D4D090E32363E1817F737EDDE828184CFA95DA084A94C6CD59053CDDB19E',
+    },
+    nodes: [
+        { friendlyName: 'TWIX-dual-0', roles: 2, url: 'http://20.52.157.15:3000' },
+        { friendlyName: 'TWIX-dual-1', roles: 2, url: 'http://20.52.233.206:3000' },
+    ],
+};
+
 
 const defaultNetworkConfig: Record<number, NetworkConfig> = {
     152: defaultTestnetNetworkConfig,
+    104: defaultMainnetNetworkConfig,
 };
 
 const resolvedNetworkConfig: NetworkConfig = window['networkConfig'] || defaultNetworkConfig;
